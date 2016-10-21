421a9d62ccc6bebc82a190650b2b8e4cbb722e73
J. Ryan Conklin <jryanconklin@gmail.com>
2016-10-21 13:51:08 -0700

    Revise Styling on Add Form.

--- app/meal-add.component.ts
+++ app/meal-add.component.ts
@@ -8,23 +8,40 @@ import { Meal } from './meal.model';
     <hr>
     <h3>Add Meal</h3>
     <form class="form-inline">
+
       <div class="form-group">
-        <label>Name</label>
-        <input #newName>
+        <div class="col-md-3">
+          <label>Name</label>
+        </div>
+        <div class="col-md-9">
+          <input #newName>
+        </div>
       </div>
+
       <div class="form-group">
-        <label>Details</label>
-        <input #newDetails>
+        <div class="col-md-3">
+          <label>Details</label>
+        </div>
+        <div class="col-md-9">
+          <input #newDetails>
+        </div>
       </div>
+
       <div class="form-group">
-        <label>Calories</label>
-        <input #newCalories>
+        <div class="col-md-3">
+          <label>Calories</label>
+        </div>
+        <div class="col-md-9 ">
+          <input #newCalories>
+        </div>
       </div>
+
       <button class="btn btn-success" (click)="addMealClicked(newName.value, newDetails.value, newCalories.value);
-        newName.value ='';
-        newDetails.value='';
-        newCalories.value='';
-        ">Add Meal</button>
+          newName.value ='';
+          newDetails.value='';
+          newCalories.value='';
+      ">Add Meal</button>
+
     </form>
     <hr>
   `
